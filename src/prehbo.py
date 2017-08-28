5d1d8780802d1be28c60d4c7f174afd08b813a2d
jeroenweber <jeroen.weber@hu.nl>
2017-08-28 10:06:40 +0200

    testje

---
+++ src/prehbo.py
@@ -0,0 +1,17 @@
+zin = "De finale van het EK is op 16 juli en begint om 21:00 uur"
+
+print("{0} {1}".format(zin[0:9],zin[-19:]))
+
+#aantal = eval(input("Hoeveel regels?"))
+
+#getal = eval(input("Welk getal?"))
+
+#for i in range(0,aantal):
+ #   print(getal)
+
+recept = {'gehakt': '600 gram', 'saus': 'veel'}
+
+recept['spaghetti'] = '750 gram'
+
+print(recept)
+
